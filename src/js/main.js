636e4ebc30c1f96c22f7af1e69ff12262e9071cb
Melvin Salas <msalas@coinca.tv>
2017-05-10 13:37:34 -0600

    Index changer

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,34 @@
 var btnNotes = document.getElementById("btn-notes");
 var notesSection = document.getElementById("section-notes");
+var btnNotebooks = document.getElementById("btn-notebooks");
+var notebooksSection = document.getElementById("section-notebooks");
+var btnTags = document.getElementById("btn-tags");
+var tagsSection = document.getElementById("section-tags");
+var oActive = { active: notesSection };
 
 btnNotes.addEventListener("click", function () {
-    console.log("TRIGGERED TOGGLE");
+    toggleHide(oActive.active);
+    oActive.active = notesSection;
     toggleHide(notesSection);
 });
 
+btnNotebooks.addEventListener("click", function () {
+    toggleHide(oActive.active);
+    oActive.active = notebooksSection;
+    toggleHide(notebooksSection);
+});
+
+btnTags.addEventListener("click", function () {
+    toggleHide(oActive.active);
+    oActive.active = tagsSection;
+    toggleHide(tagsSection);
+});
+
 function toggleHide(pSection) {
+    console.log(oActive.active);
     if (pSection.classList.contains("hide2")) {
         pSection.classList.remove("hide2");
-        pSection.style.left='';
     } else {
-        pSection.style.left='-100px';
         pSection.classList.add("hide2");
     }
 }
