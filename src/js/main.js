dee2e60a1237bfd76e0200cd680ce5d700a58ddc
luisdanielmv <luisdanielmv@gmail.com>
2017-05-11 11:07:53 -0600

    further Js changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,78 +1,3 @@
-//BrainNotes Contructor
-function BrainNotes(pSettings) {
-	var self = this;
-	self.settings = {
-		notesSettings: {
-			notesBtn: pSettings.notesSettings.notesBtn || "defaultPotato",
-			notesSection: pSettings.notesSettings.notesSection || "defaultPotato",
-			notesHolderId: pSettings.notesSettings.notesHolderId || "defaultPotato",
-			notesList: pSettings.notesSettings.notesList || "defaultPotato"
-		},
-		noteBookSettings: {
-			noteBookBtn: pSettings.noteBookSettings.noteBookBtn || "defaultPotato",
-			noteBookSection: pSettings.noteBookSettings.noteBookSection || "defaultPotato",
-			noteBookHolderId: pSettings.noteBookSettings.noteBookHolderId || "defaultPotato"
-		},
-		tagsSettings: {
-			tagsBtn: pSettings.tagsSettings.tagsBtn || "defaultPotato",
-			tagsSection: pSettings.tagsSettings.tagsSection || "defaultPotato",
-			tagsHolderId: pSettings.tagsSettings.tagsHolderId || "defaultPotato"
-		},
-		toggledClass: pSettings.toggledClass || "defaultPotato",
-		contentTitle: pSettings.contentTitle || "defaultPotato",
-		contentDisplay: pSettings.contentDisplay || "defaultPotato"
-	};
-
-	self.contentTitle = document.getElementById(self.settings.contentTitle);
-	self.contentDisplay = document.getElementById(self.settings.contentDisplay);
-
-	self.noteBtn = document.getElementById(self.settings.notesSettings.notesBtn);
-	self.noteSection = document.getElementById(self.settings.notesSettings.notesSection);
-	self.noteHolder = document.getElementById(self.settings.notesSettings.notesHolderId);
-	self.noteList = new Array();
-
-	self.noteBookBtn = document.getElementById(self.settings.noteBookSettings.noteBookBtn);
-	self.noteBookSection = document.getElementById(self.settings.noteBookSettings.noteBookSection);
-	self.noteBookList = new Array();
-
-	self.tagsBtn = document.getElementById(self.settings.tagsSettings.tagsBtn);
-	self.tagsSection = document.getElementById(self.settings.tagsSettings.tagsSection);
-	self.tagList = new Array();
-	self.active = self.noteSection;
-
-	function toggleHide(pSection) {
-		if (self.active != pSection) {
-			self.active.classList.add(self.settings.toggledClass);
-			self.active = pSection;
-			self.active.classList.remove(self.settings.toggledClass);
-		}
-	}
-
-	self.addNote = function (pTitle, pContent) {
-		var noteTemp = new Note(self.noteList.length, pTitle, pContent);
-		console.log(self.noteHolder);
-		self.noteList.push(noteTemp);
-		self.noteHolder.appendChild(noteTemp.element);
-
-		noteTemp.element.addEventListener("click", function () {
-			self.contentTitle.innerHTML = noteTemp.title;
-			self.contentDisplay.innerHTML = noteTemp.content;
-		});
-	}
-
-	self.noteBtn.addEventListener("click", function () {
-		toggleHide(self.noteSection);
-	});
-	self.noteBookBtn.addEventListener("click", function () {
-		toggleHide(self.noteBookSection);
-	});
-	self.tagsBtn.addEventListener("click", function () {
-		toggleHide(self.tagsSection);
-	});
-
-	console.log("Create new BN");
-}
-
 //Note Contructor
 function Note(pId, pTitle, pContent) {
 	var self = this;
@@ -135,10 +60,121 @@ function Tag(){
 	console.log(potato);
 }
 
+//BrainNotes Contructor
+function BrainNotes(pSettings) {
+	var self = this;
+	self.settings = {
+		notesSettings: {
+			notesBtn: pSettings.notesSettings.notesBtn || "defaultPotato",
+			notesEditBtn: pSettings.notesSettings.notesEditBtn || "defaultPotato",
+			notesSaveEditBtn: pSettings.notesSettings.notesSaveEditBtn || "defaultPotato",
+			notesSection: pSettings.notesSettings.notesSection || "defaultPotato",
+			notesHolderId: pSettings.notesSettings.notesHolderId || "defaultPotato",
+			notesList: pSettings.notesSettings.notesList || "defaultPotato"
+		},
+		noteBookSettings: {
+			noteBookBtn: pSettings.noteBookSettings.noteBookBtn || "defaultPotato",
+			noteBookSection: pSettings.noteBookSettings.noteBookSection || "defaultPotato",
+			noteBookHolderId: pSettings.noteBookSettings.noteBookHolderId || "defaultPotato"
+		},
+		tagsSettings: {
+			tagsBtn: pSettings.tagsSettings.tagsBtn || "defaultPotato",
+			tagsSection: pSettings.tagsSettings.tagsSection || "defaultPotato",
+			tagsHolderId: pSettings.tagsSettings.tagsHolderId || "defaultPotato"
+		},
+		toggledClass: pSettings.toggledClass || "defaultPotato",
+		displayNoteTitle: pSettings.displayNoteTitle || "defaultPotato",
+		displayNoteContent: pSettings.displayNoteContent || "defaultPotato"
+	};
+
+	self.displayNoteTitle = document.getElementById(self.settings.displayNoteTitle);
+	self.displayNoteContent = document.getElementById(self.settings.displayNoteContent);
+
+	self.noteBtn = document.getElementById(self.settings.notesSettings.notesBtn);
+	self.noteEditBtn = document.getElementById(self.settings.notesSettings.notesEditBtn);
+	self.noteSaveEditBtn = document.getElementById(self.settings.notesSettings.notesSaveEditBtn);
+	self.noteSection = document.getElementById(self.settings.notesSettings.notesSection);
+	self.noteHolder = document.getElementById(self.settings.notesSettings.notesHolderId);
+	self.noteList = new Array();
+
+	self.noteBookBtn = document.getElementById(self.settings.noteBookSettings.noteBookBtn);
+	self.noteBookSection = document.getElementById(self.settings.noteBookSettings.noteBookSection);
+	self.noteBookList = new Array();
+
+	self.tagsBtn = document.getElementById(self.settings.tagsSettings.tagsBtn);
+	self.tagsSection = document.getElementById(self.settings.tagsSettings.tagsSection);
+	self.tagList = new Array();
+	self.active = self.noteSection;
+	self.activeNote = "Default Potato";
+
+	function getElementById(pId) {
+		var tempElement = document.getElementById(pId);
+		return tempElement;
+	}
+
+	function toggleHide(pSection) {
+		if (self.active != pSection) {
+			self.active.classList.add(self.settings.toggledClass);
+			self.active = pSection;
+			self.active.classList.remove(self.settings.toggledClass);
+		}
+	}
+
+	self.addNote = function (pTitle, pContent) {
+		var noteTemp = new Note(self.noteList.length, pTitle, pContent);
+		console.log(self.noteHolder);
+		self.noteList.push(noteTemp);
+		self.noteHolder.appendChild(noteTemp.element);
+
+		noteTemp.element.addEventListener("click", function () {
+			self.displayNoteTitle.innerHTML = noteTemp.title;
+			self.displayNoteContent.innerHTML = noteTemp.content;
+			self.activeNote = noteTemp;
+		});
+	}
+
+	displayNote(pNote) {
+		var txtNodeTitle = document.createTextNode(pNote.title),
+			txtNodeContent = document.createTextNode(pNote.content);
+
+		self.displayNoteTitle.removeChild(self.displayNoteTitle.firstChild);
+		self.displayNoteTitle.appentChild(txtNodeTitle);
+		self.displayNoteContent.removeChild(self.displayNoteContent.firstChild);
+		self.displayNoteContent.appentChild(txtNodeContent);	
+	}
+
+	editNote = function () {
+		self.displayNoteTitle.setAttribute("contenteditable", "true");
+		self.displayNoteContent.setAttribute("contenteditable", "true");
+	}
+
+	saveEdit = function () {
+		self.activeNote.title = self.displayNoteTitle.nodeValue;
+		self.activeNote.content = self.displayNoteContent.nodeValue;
+		self.displayNoteTitle.removeAttribute("contenteditable");
+		self.displayNoteContent.removeAttribute("contenteditable");
+		self.displayNote(self.activeNote);
+	}
+
+	self.noteBtn.addEventListener("click", function () {
+		toggleHide(self.noteSection);
+	});
+	self.noteBookBtn.addEventListener("click", function () {
+		toggleHide(self.noteBookSection);
+	});
+	self.tagsBtn.addEventListener("click", function () {
+		toggleHide(self.tagsSection);
+	});
+
+	console.log("Create new BN");
+}
+
 //BrainNotes Initialization
-var BN = new BrainNotes({
+var bn = new BrainNotes({
 	notesSettings: {
 		notesBtn: "btn-notes",
+		notesEditBtn: "btn-edit-notes",
+		notesSaveEditBtn: "btn-save-notes",
 		notesSection: "section-notes",
 		notesHolderId: "holder-notes",
 		notesList: [{title:"title1", content:"content1"}, {title:"title2", content:"content2"}, {title:"title3", content:"content3"}]
@@ -154,11 +190,11 @@ var BN = new BrainNotes({
 		tagsHolderId: ""
 	},
 	toggledClass: "hide2",
-	contentTitle: "content-title",
-	contentDisplay: "content-display"
+	displayNoteTitle: "content-title",
+	displayNoteContent: "content-display"
 });
 
-BN.addNote("Titulo Test 1", "Content Test 1");
-BN.addNote("Titulo Test 2", "Content Test 2");
+bn.addNote("Titulo Test 1", "Content Test 1");
+bn.addNote("Titulo Test 2", "Content Test 2");
 
 console.log(BN.constructor.name);
